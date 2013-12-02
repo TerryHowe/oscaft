ccc3f65b2c78403338c04e94b053a024354cb8d9
Terry Howe <terrylhowe@gmail.com>
2013-12-02 13:10:39 -0700

    make code comply with various rules

--- port.py
+++ port.py
@@ -1,6 +1,22 @@
+#   Copyright 2013 OpenStack, LLC.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
 import pexpect
 import sys
 import traceback
+
 
 def main():
     rc = 1
@@ -14,7 +30,8 @@ def main():
         child.expect('id="(.*)"')
         netid = child.match.group(1)
         child.expect(prompt)
-        child.sendline('port create lucky --network ' + netid + ' -f shell -c id')
+        child.sendline('port create lucky --network ' + netid +
+                       ' -f shell -c id')
         child.expect('id="(.*)"')
         id = child.match.group(1)
         child.expect(prompt)
@@ -30,7 +47,7 @@ def main():
         child.sendline("port delete lucky")
         child.expect('Deleted port: lucky')
         child.expect(prompt)
-        print "SUCCESS"
+        print("SUCCESS")
         rc = 0
     except Exception:
         traceback.print_exc(file=sys.stdout)
--- security_group.py
+++ security_group.py
@@ -1,6 +1,22 @@
+#   Copyright 2013 OpenStack, LLC.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
 import pexpect
 import sys
 import traceback
+
 
 def main():
     rc = 1
@@ -10,7 +26,8 @@ def main():
         child.expect(prompt)
         child.sendline("security group delete lucky")
         child.expect(prompt)
-        child.sendline('security group create lucky --description oscaft -f shell -c id')
+        child.sendline('security group create lucky --description oscaft'
+                       ' -f shell -c id')
         child.expect('id="(.*)"')
         id = child.match.group(1)
         child.expect(prompt)
@@ -20,13 +37,14 @@ def main():
         child.sendline("security group list -f csv -c id -c name")
         child.expect("\"" + id + "\",\"lucky\"")
         child.expect(prompt)
-        child.sendline("security group set --description OSCAFT --name lucky " + id)
+        child.sendline("security group set --description OSCAFT"
+                       " --name lucky " + id)
         child.expect('Updated security_group: ' + id)
         child.expect(prompt)
         child.sendline("security group delete lucky")
         child.expect('Deleted security_group: lucky')
         child.expect(prompt)
-        print "SUCCESS"
+        print("SUCCESS")
         rc = 0
     except Exception:
         traceback.print_exc(file=sys.stdout)
